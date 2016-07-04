6e097a0c8267667a4defdf21c15e6b4e2ca4ee29
Quentin LD <quentin.ledoledec@gmail.com>
2016-07-04 22:03:49 +0200

    misc(css): preventing user zoom

--- webroot/index.php
+++ webroot/index.php
@@ -11,11 +11,11 @@
         <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
         <meta charset=utf-8>
         <meta http-equiv=X-UA-Compatible content="IE=edge">
-        <meta name=viewport content="width=device-width,initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 
         <link href="/css/bootstrap.min.css" rel="stylesheet" />
         <link href="/css/todc-bootstrap.min.css" rel="stylesheet" />
-        <link href="/css/ribbon.min.css" rel="stylesheet" />
+        <!-- <link href="/css/ribbon.min.css" rel="stylesheet" /> -->
         <link href="/css/mapicoin.css?<?= VERSION ?>" rel="stylesheet" />
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
