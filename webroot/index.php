c076fed15549fad6965e4784e7f6270379f454f7
Quentin LD <quentin.ledoledec@gmail.com>
2016-07-19 14:34:21 +0200

    fix(mobile): removing force viewport scale

--- webroot/index.php
+++ webroot/index.php
@@ -11,7 +11,7 @@
         <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
         <meta charset=utf-8>
         <meta http-equiv=X-UA-Compatible content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" /> -->
 
         <link href="/css/bootstrap.min.css" rel="stylesheet" />
         <link href="/css/todc-bootstrap.min.css" rel="stylesheet" />
