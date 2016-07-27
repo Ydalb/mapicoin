84b246b46f6901d7f73aadb5a1702da3ab1680c7
Quentin LD <quentin.ledoledec@gmail.com>
2016-07-27 22:07:34 +0200

    fix(responsive): small changes

--- webroot/inc/header.inc.php
+++ webroot/inc/header.inc.php
@@ -1,7 +1,7 @@
     <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
     <meta charset=utf-8>
     <meta http-equiv=X-UA-Compatible content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, minimal-ui" />
     <link rel="icon" href="favicon.ico?<?= VERSION ?>" />
     <!-- <link href="/css/ribbon.min.css" rel="stylesheet" /> -->
     <!-- <link href="/css/bootstrap.min.css" rel="stylesheet" /> -->
