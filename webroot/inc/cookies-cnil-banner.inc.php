75c911a7340b2d844052f75a60aa1903c5b56d82
Quentin LD <quentin.ledoledec@gmail.com>
2016-08-01 18:31:41 +0200

    feat(cnil): adding CNIL banner (bis)

---
+++ webroot/inc/cookies-cnil-banner.inc.php
@@ -0,0 +1,5 @@
+<div id="cookies-cnil-banner" style="display: none;">
+    <p>En poursuivant votre navigation sur ce site, vous acceptez l'utilisation de cookies par Google Analytics pour réaliser des statistiques de visites. <a href="./en-savoir-plus.html" id="cookies-cnil-more">En savoir plus</a></p>
+    <button class="hand" id="cookies-cnil-reject">Je refuse</button>
+    <button class="hand" id="cookies-cnil-accept">J'accepte</button>
+</div>
